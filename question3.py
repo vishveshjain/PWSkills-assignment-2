799454bdce894ab47a151769323de8d64f8691ae
Vishvesh Jain <vishvesh2851994@gmail.com>
2023-02-02 01:33:14 +0530

    q3
    with comments

---
+++ question3.py
@@ -0,0 +1,11 @@
+#accept any city from the user and display monuments of that city
+
+#user input
+city = input("Enter city name: ")
+#display monuments of that city
+if city == "Delhi":
+    print("Red Fort")
+elif city == "Agra":
+    print("Taj Mahal")
+elif city == "Jaipur":
+    print("Jal Mahal")
