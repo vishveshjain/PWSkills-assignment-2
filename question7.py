b45d6849aa13c1cb04b53db0f0f7f28752776ebf
Vishvesh Jain <vishvesh2851994@gmail.com>
2023-02-02 02:29:06 +0530

    q7
    With comments

---
+++ question7.py
@@ -0,0 +1,11 @@
+#reverse a while loop to display the numbers from 10 to 1
+
+#variable declaration
+upperBound=10
+lowerBound=1
+#while loop
+while upperBound>=lowerBound:
+    #print upperBound
+    print(upperBound)
+    #decrement upperBound by 1
+    upperBound-=1
